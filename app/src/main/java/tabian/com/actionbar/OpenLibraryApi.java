88c4e60e88c497f47ffdaa351b7a1ee9bca75265
JavaCoffeeBean <JavaCoffeeBean@gmail.com>
2020-01-06 20:27:51 -0600

    ready to move request to ScanActivity

--- app/src/main/java/tabian/com/actionbar/OpenLibraryApi.java
+++ app/src/main/java/tabian/com/actionbar/OpenLibraryApi.java
@@ -7,7 +7,7 @@ import retrofit2.http.Query;
 
 public interface OpenLibraryApi {
 
-    @GET("books/v1/volumes?key=AIzaSyDG6IF6-n0EDhvSfac4OYNqtU520xQUqzQ&")
+    @GET("books/v1/volumes?&key=AIzaSyDG6IF6-n0EDhvSfac4OYNqtU520xQUqzQ")
     Call<Post> getPosts(@Query("q") long isbnCode);
 
 }
